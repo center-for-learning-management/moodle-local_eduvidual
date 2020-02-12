0500e886c68ee0b2073920d311e8c34f9acb80f3
Robert Schrenk <robert@schrenk.cc>
2020-02-12 13:21:52 +0100

    Hotfix: Redirect privacy issues also for other contexts.

--- lib.php
+++ lib.php
@@ -128,7 +128,7 @@ function block_eduvidual_before_standard_html_head() {
                     $html .= '</a></p>';
                     $injects[] = $html;
                 }
-            } elseif ($PAGE->context->contextlevel == CONTEXT_COURSECAT) {
+            } else {
                 block_eduvidual::redirect_privacy_issue('context-' . $PAGE->context->id);
             }
         }
