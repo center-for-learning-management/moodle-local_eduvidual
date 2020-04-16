dc0680fd4c94dde89d91e9d68e6b474a905860ff
Robert Schrenk <robert@schrenk.cc>
2020-04-17 00:59:05 +0200

    Bugfix course basements do not depend on database anymore.

--- ajax/sub/register.php
+++ ajax/sub/register.php
@@ -107,6 +107,7 @@ if ($USER->id > 1) {
              * Check registration token for this org and create if correct
              * Should return if token was correct and organisation was created
             */
+
             $orgid = optional_param('orgid', -1, PARAM_INT);
             $authtan = optional_param('token', '', PARAM_TEXT);
             $name = substr(optional_param('name', '', PARAM_TEXT), 0, 30);
@@ -122,116 +123,130 @@ if ($USER->id > 1) {
 
                 if (isset($org->orgid) && $org->orgid == $orgid) {
                     $org->name = $name;
-                    $org->authenticated = 1;
-                    $org->authtan = '';
+                    $DB->set_field('block_eduvidual_org', 'name', $name, array('orgid' => $org->orgid));
 
                     require_once($CFG->dirroot . '/lib/coursecatlib.php');
                     require_once($CFG->dirroot . '/course/externallib.php');
 
-                    // Create a course category for this org
-                    $data = new \stdClass();
-                    $data->name = $org->name;
-                    $data->description = $org->name;
-                    $data->idnumber = $org->orgid;
-                    $category = coursecat::create($data);
-                    $org->categoryid = $category->id;
+                    if (empty($org->categoryid)) {
+                        // Create a course category for this org
+                        $data = new \stdClass();
+                        $data->name = $org->name;
+                        $data->description = $org->name;
+                        $data->idnumber = $org->orgid;
+                        $category = coursecat::create($data);
+                        $org->categoryid = $category->id;
+                        $DB->set_field('block_eduvidual_org', 'categoryid', $org->categoryid, array('orgid' => $org->orgid));
+                    }
 
                     $reply["name"] = $org->name;
                     $reply["categoryid"] = $org->categoryid;
 
-                    // Create an org-course for this org
-                    $orgcoursebasement = get_config('block_eduvidual', 'orgcoursebasement');
-                    $basement = $DB->get_record('block_eduvidual_basements', array('courseid' => $orgcoursebasement));
-                    if ($basement->courseid > 0) {
-                        // Grant a role that allows course duplication in source and target category
-                        $basecategory = $DB->get_record('course', array('id' => $basement->courseid));
-                        $sourcecontext = context_coursecat::instance($basecategory->category);
-                        $targetcontext = context_coursecat::instance($org->categoryid);
-                        $roletoassign = 1; // Manager
-                        $revokesourcerole = true;
-                        $revoketargetrole = true;
-                        $roles = get_user_roles($sourcecontext, $USER->id, false);
-                        foreach($roles AS $role) {
-                            if ($role->roleid == $roletoassign) {
-                                // User had this role before - we do not revoke!
-                                $revokesourcerole = false;
-                                $reply['msgs'][] = 'Has already been manager in source';
+                    if (empty($org->courseid)) {
+                        // Create an org-course for this org
+                        $orgcoursebasement = get_config('block_eduvidual', 'orgcoursebasement');
+                        $basement = $DB->get_record('course', array('id' => $orgcoursebasement));
+
+                        $reply["orgcourse"] = $basement;
+                        if (!empty($basement->id)) {
+                            // Grant a role that allows course duplication in source and target category
+                            $sourcecontext = context_coursecat::instance($basement->category);
+                            $targetcontext = context_coursecat::instance($org->categoryid);
+                            $roletoassign = 1; // Manager
+                            $revokesourcerole = true;
+                            $revoketargetrole = true;
+                            $roles = get_user_roles($sourcecontext, $USER->id, false);
+                            foreach($roles AS $role) {
+                                if ($role->roleid == $roletoassign) {
+                                    // User had this role before - we do not revoke!
+                                    $revokesourcerole = false;
+                                    $reply['msgs'][] = 'Has already been manager in source';
+                                }
                             }
-                        }
-                        $roles = get_user_roles($targetcontext, $USER->id, false);
-                        foreach($roles AS $role) {
-                            if ($role->roleid == $roletoassign) {
-                                // User had this role before - we do not revoke!
-                                $revoketargetrole = false;
-                                $reply['msgs'][] = 'Has already been manager in target';
+                            $roles = get_user_roles($targetcontext, $USER->id, false);
+                            foreach($roles AS $role) {
+                                if ($role->roleid == $roletoassign) {
+                                    // User had this role before - we do not revoke!
+                                    $revoketargetrole = false;
+                                    $reply['msgs'][] = 'Has already been manager in target';
+                                }
                             }
-                        }
-                        $reply['msgs'][] = 'Assigning role manager in source/target';
-                        role_assign($roletoassign, $USER->id, $sourcecontext->id);
-                        role_assign($roletoassign, $USER->id, $targetcontext->id);
+                            $reply['msgs'][] = 'Assigning role manager in source/target';
+                            role_assign($roletoassign, $USER->id, $sourcecontext->id);
+                            role_assign($roletoassign, $USER->id, $targetcontext->id);
 
-                        // Duplicate course
-                        $course = core_course_external::duplicate_course($basement->courseid, 'Digitaler Schulhof (' . $org->orgid . ')', $org->orgid, $org->categoryid, true);
-                        $org->courseid = $course["id"];
-                        $course['summary'] = '<p>Digitaler Schulhof der Schule ' . $org->name . '</p>';
-                        $DB->update_record('course', $course);
+                            // Duplicate course
+                            $course = core_course_external::duplicate_course($basement->id, 'Digitaler Schulhof (' . $org->orgid . ')', $org->orgid, $org->categoryid, true);
+                            $org->courseid = $course["id"];
+                            $DB->set_field('block_eduvidual_org', 'courseid', $org->courseid, array('orgid' => $org->orgid));
+                            $course['summary'] = '<p>Digitaler Schulhof der Schule ' . $org->name . '</p>';
+                            $DB->update_record('course', $course);
 
-                        // Revoke role that allows course duplication in source and target category
-                        if ($revokesourcerole) {
-                            role_unassign($roletoassign, $USER->id, $sourcecontext->id);
-                            $reply['msgs'][] = 'Revoke role from source';
-                        }
-                        if ($revoketargetrole) {
-                            role_unassign($roletoassign, $USER->id, $targetcontext->id);
-                            $reply['msgs'][] = 'Revoke role from target';
-                        }
-                    }
-                    require_once($CFG->dirroot . '/blocks/eduvidual/classes/lib_enrol.php');
-                    $reply['roleset'] = block_eduvidual_lib_enrol::role_set($USER->id, $org, 'Manager');
-
-                    $DB->update_record('block_eduvidual_org', $org);
-
-                    $messagehtml = $OUTPUT->render_from_template(
-                        'block_eduvidual/register_mail_completed',
-                        (object) array(
-                            'categoryurl' => $CFG->wwwroot . '/course/index.php?categoryid=' . $org->categoryid,
-                            'managerscourseurl' => $CFG->wwwroot . '/course/view.php?id=398',
-                            'orgid' => $org->orgid,
-                            'orgname' => $org->name,
-                            'subject' => get_string('mailregister:2:header', 'block_eduvidual'),
-                            'wwwroot' => $CFG->wwwroot,
-                        )
-                    );
-
-                    $messagetext = html_to_text($messagehtml);
-
-                    $subject = get_string('mailregister:2:subject' , 'block_eduvidual');
-
-                    $fromuser = core_user::get_support_user();
-
-                    $touser = new \stdClass();
-                    $touser->email = '';
-                    $touser->firstname = 'Registration';
-                    $touser->lastname = 'Completed';
-                    $touser->maildisplay = true;
-                    $touser->mailformat = 1; // 0 (zero) text-only emails, 1 (one) for HTML/Text emails.
-                    $touser->id = -99; // invalid userid, as the user has no userid in our moodle.
-                    $touser->firstnamephonetic = "";
-                    $touser->lastnamephonetic = "";
-                    $touser->middlename = "";
-                    $touser->alternatename = "";
-
-                    // Sending a short statement to CC-Users
-                    $ccmails = explode(',', get_config('block_eduvidual', 'registrationcc'));
-                    if (count($ccmails) > 0) {
-                        foreach($ccmails AS $ccmail) {
-                            if (!filter_var($ccmail, FILTER_VALIDATE_EMAIL)) continue;
-                            $touser->email = trim($ccmail);
-                            email_to_user($touser, $fromuser, $subject, $messagetext, $messagehtml, "", true);
+                            // Revoke role that allows course duplication in source and target category
+                            if ($revokesourcerole) {
+                                role_unassign($roletoassign, $USER->id, $sourcecontext->id);
+                                $reply['msgs'][] = 'Revoke role from source';
+                            }
+                            if ($revoketargetrole) {
+                                role_unassign($roletoassign, $USER->id, $targetcontext->id);
+                                $reply['msgs'][] = 'Revoke role from target';
+                            }
                         }
                     }
 
-                    $reply['status'] = 'ok';
+                    if (!empty($org->courseid)) {
+                        require_once($CFG->dirroot . '/blocks/eduvidual/classes/lib_enrol.php');
+                        $reply['roleset'] = block_eduvidual_lib_enrol::role_set($USER->id, $org, 'Manager');
+
+                        $messagehtml = $OUTPUT->render_from_template(
+                            'block_eduvidual/register_mail_completed',
+                            (object) array(
+                                'categoryurl' => $CFG->wwwroot . '/course/index.php?categoryid=' . $org->categoryid,
+                                'managerscourseurl' => $CFG->wwwroot . '/course/view.php?id=398',
+                                'orgid' => $org->orgid,
+                                'orgname' => $org->name,
+                                'subject' => get_string('mailregister:2:header', 'block_eduvidual'),
+                                'wwwroot' => $CFG->wwwroot,
+                            )
+                        );
+
+                        $messagetext = html_to_text($messagehtml);
+
+                        $subject = get_string('mailregister:2:subject' , 'block_eduvidual');
+
+                        $fromuser = core_user::get_support_user();
+
+                        $touser = new \stdClass();
+                        $touser->email = '';
+                        $touser->firstname = 'Registration';
+                        $touser->lastname = 'Completed';
+                        $touser->maildisplay = true;
+                        $touser->mailformat = 1; // 0 (zero) text-only emails, 1 (one) for HTML/Text emails.
+                        $touser->id = -99; // invalid userid, as the user has no userid in our moodle.
+                        $touser->firstnamephonetic = "";
+                        $touser->lastnamephonetic = "";
+                        $touser->middlename = "";
+                        $touser->alternatename = "";
+
+                        // Sending a short statement to CC-Users
+                        $ccmails = explode(',', get_config('block_eduvidual', 'registrationcc'));
+                        if (count($ccmails) > 0) {
+                            foreach($ccmails AS $ccmail) {
+                                if (!filter_var($ccmail, FILTER_VALIDATE_EMAIL)) continue;
+                                $touser->email = trim($ccmail);
+                                email_to_user($touser, $fromuser, $subject, $messagetext, $messagehtml, "", true);
+                            }
+                        }
+                        $org->authenticated = 1;
+                        $org->authtan = '';
+                        $DB->set_field('block_eduvidual_org', 'authenticated', 1, array('orgid' => $org->orgid));
+                        $DB->set_field('block_eduvidual_org', 'authtan', '', array('orgid' => $org->orgid));
+                        $reply['status'] = 'ok';
+                    } else {
+                        $reply['status'] = 'error';
+                        $reply['error'] = 'error creating category or course';
+                    }
+
                 } else {
                     $reply['status'] = 'error';
                 }
