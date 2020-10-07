629ed4e9ad5adfbdb2bf27a408644dac2784bfa3
Robert Schrenk <robert@schrenk.cc>
2020-10-07 09:30:28 +0200

    PODS hook

--- pages/hooks/pods.php
+++ pages/hooks/pods.php
@@ -20,9 +20,90 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+defined('MOODLE_INTERNAL') || die();
+
 /**
  * This program is called directly within the login-procedure of auth_shibboleth_link.
  * It receives all profile data from the Shibboleth-Login and managers enrolments to organisations.
  */
 
-print_r($_SESSION);
+if (empty($idpparams['userinfo']['username'])) return;
+
+global $DB, $USER;
+
+// 1.) If data is missing - fill with random data and store back to cache.
+if (empty($idpparams['userinfo']['firstname'])) {
+    $colors = file($CFG->dirroot . '/local/eduvidual/templates/names.colors', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $color_key = array_rand($colors, 1);
+    $idpparams['userinfo']['firstname'] = $colors[$color_key];
+}
+if (empty($idpparams['userinfo']['lastname'])) {
+    $animals = file($CFG->dirroot . '/local/eduvidual/templates/names.animals', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $animal_key = array_rand($animals, 1);
+    $idpparams['userinfo']['lastname'] = $animals[$animal_key];
+}
+if (empty($idpparams['userinfo']['email'])) {
+    $dummydomain = \local_eduvidual\locallib::get_dummydomain();
+    $pattern = 'e-' . date("Ym") . '-';
+    $usernameformat= $pattern . '%1$04d';
+    $lasts = $DB->get_records_sql('SELECT username FROM {user} WHERE username LIKE ? ORDER BY username DESC LIMIT 0,1', array($pattern . '%'));
+    if ((count($lasts)) > 0) {
+        foreach($lasts AS $last){
+            $usernumber = intval(str_replace($pattern, '', $last->username)) + 1;
+        }
+    } else {
+        $usernumber = 1;
+    }
+
+    $fictiveusername = sprintf($usernameformat, $usernumber++);
+    $idpparams['userinfo']['email'] = $fictiveusername . $dummydomain;
+}
+
+
+\auth_shibboleth_link\lib::link_data_store_cache($idpparams);
+
+// 2.) if institution is given, determine role and enrol to organization
+
+if (!empty($idpparams['userinfo']['institution'])) {
+    $role = 'Student';
+    if (!empty($idpparams['userinfo']['appList'])) {
+         $applist = explode('\;', $idpparams['userinfo']['appList']);
+         foreach ($applist as $app) {
+             $approle = explode('@', $app);
+             if (count($approle) > 1 && $approle[1] == 'EVI') {
+                 switch ($approle[0]) {
+                     case 'std': $role = 'Student'; break;
+                     case 'tch': $role = 'Teacher'; break;
+                     case 'adm': $role = 'Manager'; break;
+                 }
+             }
+         }
+    }
+    $org = $DB->get_record('local_eduvidual_org', array('orgid' => $idpparams['userinfo']['institution']));
+
+    if (!empty($org->id) && !empty($org->authenticated)) {
+        $membership = $DB->get_record('local_eduvidual_orgid_userid', array('orgid' => $idpparams['userinfo']['institution'], 'userid' => $USER->id));
+        if (empty($membership->role)) {
+            \local_eduvidual\lib_enrol::role_set($USER->id, $org, $role);
+        } else {
+            switch ($membership->role) {
+                // Manager cannot get better - keep...
+                case 'Teacher':
+                    if (in_array($role, array('Manager'))) {
+                        \local_eduvidual\lib_enrol::role_set($USER->id, $org, $role);
+                    }
+                break;
+                case 'Student':
+                case 'Parent':
+                    if (in_array($role, array('Manager', 'Teacher'))) {
+                        \local_eduvidual\lib_enrol::role_set($USER->id, $org, $role);
+                    }
+                break;
+            }
+        }
+        // 3.) If there is a department create a cohort for that department
+        if (!empty($idpparams['userinfo']['department'])) {
+            \local_eduvidual\lib_enrol::cohorts_add($USER->id, $org, $idpparams['userinfo']['department']);
+        }
+    }
+}
