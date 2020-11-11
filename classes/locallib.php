83e9919a88ca9b0ec3cd7bf290a12621aa20610f
Robert Schrenk <robert@schrenk.cc>
2020-11-11 14:18:36 +0100

    Bugfix setting header

--- classes/locallib.php
+++ classes/locallib.php
@@ -335,12 +335,12 @@ class locallib {
                         LIMIT 0,1";
             $primaryorg = $DB->get_record_sql($sql);
             if (!empty($primaryorg->orgid)) {
-                header('x-orgid', $primaryorg->orgid);
-                header('x-orgclass', $primaryorg->orgclass);
+                header('x-orgid: ' . $primaryorg->orgid);
+                header('x-orgclass: ' . $primaryorg->orgclass);
                 setcookie('x-orgclass', $primaryorg->orgclass, 0,'/');
             } else {
-                header('x-orgid', '0');
-                header('x-orgclass', '0');
+                header('x-orgid: 0');
+                header('x-orgclass: 0');
                 setcookie('x-orgclass', '0', 0,'/');
             }
         }
