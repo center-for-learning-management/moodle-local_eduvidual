460de986a63b1c557a045f67507c2c43663e181a
Robert Schrenk <robert@schrenk.cc>
2021-04-21 12:27:03 +0200

    escaping underscore

--- classes/lib_import.php
+++ classes/lib_import.php
@@ -333,11 +333,11 @@ class local_eduvidual_lib_import_compiler_user extends local_eduvidual_lib_impor
         $sql = "SELECT id
                     FROM {user}
                     WHERE
-                        username LIKE ? OR
-                        email LIKE ?";
+                        username LIKE ? ESCAPE '^' OR
+                        email LIKE ? ESCAPE '^'";
         $params = array(
-            str_replace('_', '[_]', $obj->email),
-            str_replace('_', '[_]', $obj->email)
+            str_replace('_', '^_', $obj->email),
+            str_replace('_', '^_', $obj->email)
         );
         $ids = array_keys($DB->get_records_sql($sql, $params));
         if (count($ids) > 0) {
