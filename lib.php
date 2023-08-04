c1d1cf8152ce0eeec83fe69f78cf9de50fceaced
prowebat <1819129+prowebat@users.noreply.github.com>
2023-08-04 09:03:30 +0200

    https://github.com/center-for-learning-management/eduvidual-src/issues/976

--- lib.php
+++ lib.php
@@ -302,7 +302,7 @@ function local_eduvidual_extend_navigation(global_navigation $navigation) {
         $custommenu .= get_string('createcourse:here', 'local_eduvidual') . "|/local/eduvidual/pages/createcourse.php\n";
     }
 
-    if (class_exists(\local_edusupport\lib) && \local_edusupport\lib::is_supportteam()) {
+    if (class_exists(\local_edusupport\lib::class) && \local_edusupport\lib::is_supportteam()) {
         $custommenu .= get_string('issues', 'local_edusupport') . "|/local/edusupport/issues.php\n";
     }
 
